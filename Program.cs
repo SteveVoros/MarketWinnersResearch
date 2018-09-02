09f214e169d054c0cfc0c91fa4836fbc46741729
SteveVoros <istvan.voros88@gmail.com>
2018-09-01 19:35:01 -0500

    Checked in initial code

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MarketWinnersResearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
